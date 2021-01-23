552603470065a3c051229bb8b1a1c2de198cdfb9
a92913 <helder.p.teixeira.22@gmail.com>
2021-01-23 08:41:03 +0000

    Backend

--- webbook/server.js
+++ webbook/server.js
@@ -3,13 +3,8 @@ const port = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
 
-
-
 app.use('/assets', express.static('assets'));
 app.use('/views', express.static('views'));
-
-var LocalStorage = require('node-localstorage').LocalStorage;
-let localStorage = LocalStorage('./scratch');
 
 //carregar bibliotecas globais
 const cors = require("cors");
@@ -26,52 +21,6 @@ app.listen(port, function(err) {
         console.log(err);
     }
 });
-
-const multer = require('multer');
-const path = require('path');
-
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        let name = Date.now() + path.extname(file.originalname);
-        cb(null, name);
-        let nameUpload = "../Backend/webbook/uploads/" + name;
-        localStorage.setItem("foto", nameUpload);
-        console.log(nameUpload)
-    }
-});
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
-        cb(null, true);
-    }
-    else {
-        cb(null, false);
-    }
-}
-const upload = multer({ storage: storage, fileFilter: fileFilter });
-
-//Upload route
-app.post('/upload', upload.single('image'), (req, res, next) => {
-    try {
-        return res.status(201).json({
-            message: 'File uploded successfully'
-        });
-    }
-    catch (error) {
-        console.error(error);
-    }
-});
-
-
-
-
-
-
-
 
 module.exports = app;
 require('./loader.js');
